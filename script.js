0a19b3423913694355677f8db9b79864d5f8ff81
Shahria Jaman Khan <41187226+shahria7k@users.noreply.github.com>
2021-02-26 21:45:26 +0600

    understook js closures

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ var warrior = "Ninja";
 const screamWarrior = () => {
 	warrior2 =
 		"Samurai"; /**If  variable declaration is removed, the variable becomes global and works outside of the its container globally */
-	console.log(warrior, warrior2);
-	console.log("function is working");
+	return {
+		shootWarrior: () => {
+			return console.log(warrior, warrior2);
+			/**
+			 * created a closure that has access to its parents variables & the global
+			 * it gives access to this local variable in the global scope when iis called.
+			 *
+			 */
+		},
+	};
 };
-screamWarrior();
-console.log("function is working");
-console.log(
-	warrior,
-	warrior2 /**this will show an error because its nested in a function and scoped locally thats why its not available outside of function */
-);
+const newWarrior = screamWarrior();
+console.log(newWarrior);
+newWarrior.shootWarrior();
+// console.log(
+// 	warrior,
+// 	warrior2
+// 	/**this will show an error because its nested in a
+//     function and scoped locally thats why its not available outside of function */
+// );
